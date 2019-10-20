bae2f4c603dc3a366bc83466b2c3efc73528cd60
Gi Soong Chee <gi.soongchee@dhs.sg>
2019-10-20 11:13:56 +0800

    added 2j ben sze contribution

--- t13_mode2.py
+++ t13_mode2.py
@@ -17,3 +17,18 @@ print("Mode: ", end='')
 for key, value in modes.items():
   if value == highest:
     print(key, end=' ')
+
+
+# Alternative solutions    
+# Contributed by 19Y2J BEN SZE HE YU
+A = [1, 2, 3, 4, 5, 5] 
+n = len(A) 
+  
+data = Counter(A) 
+get_mode = dict(data) 
+mode = [k for k, v in get_mode.items() if v == max(list(data.values()))] 
+  
+if len(mode) == n: 
+    print("No mode found")
+else: 
+    print("Mode is / are: " + ', '.join(map(str, mode))
