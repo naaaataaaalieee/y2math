45cbddea4f142676dfa16f8dba52fa2db2f1d1f0
Gi Soong Chee <gi.soongchee@dhs.sg>
2019-10-20 10:53:52 +0800

    Update and rename t13_mean.py to t13_mean1.py

--- t13_mean1.py
+++ t13_mean1.py
@@ -5,7 +5,7 @@ n = 10
 numbers = []
 
 for i in range(n):
-  numbers.append(random.randint(1,100))
+  numbers.append(random.randint(1,100)) # between 1 to 100 inclusive
 
 total = 0
 for number in numbers:
